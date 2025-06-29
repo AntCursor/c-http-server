9507b0e667e6aed8a5d27e1003761be468a2e79e
AntCursor <antcursor@gmail.com>
2025-06-29 13:59:40 -0300

    Moved allocations to start of main

--- chttp.c
+++ chttp.c
@@ -7,7 +7,6 @@
 
 #include "addrconv.h"
 #include "connection.h"
-#include "errors.h"
 #include "vector.h"
 
 #define LOCAL_HOST 0x7f000001 // 127.0.0.1
@@ -15,6 +14,8 @@
 #define BACKLOG 20 // size of connection queue
 #define DEFAULT_PORT (uint16_t)8080
 #define MSG_BUFFER_SIZE 256
+
+int exit_code = EXIT_SUCCESS;
 
 int main(int argc, char **argv) {
   uint32_t addr = LOCAL_HOST;
@@ -33,6 +34,19 @@ int main(int argc, char **argv) {
     fprintf(stderr, "Error allocating memory for socket.\n");
     return EXIT_FAILURE;
   }
+
+  SocketIPv4 *connection = malloc(sizeof(SocketIPv4));
+  if (!connection) {
+    fprintf(stderr, "Error allocating memory for connection socket.\n");
+    return EXIT_FAILURE;
+  }
+
+  CharVec *message = vector_init(MSG_BUFFER_SIZE);
+  if (!message) {
+    fprintf(stderr, "Error allocating memory for message buffer.\n");
+    return EXIT_FAILURE;
+  }
+
   if (initListenSocket(addr, port, BACKLOG, listen_socket)) {
     fprintf(stderr, "Error creating listening socket.\n");
     return EXIT_FAILURE;
@@ -40,7 +54,6 @@ int main(int argc, char **argv) {
 
   printf("Listening for connections.\n");
 
-  SocketIPv4 *connection = malloc(sizeof(SocketIPv4));
   if (acceptConnection(listen_socket, connection)) {
     fprintf(stderr, "Error accepting connection.");
     return EXIT_FAILURE;
@@ -54,7 +67,6 @@ int main(int argc, char **argv) {
   char msgBuffer[MSG_BUFFER_SIZE];
   size_t bytesRead;
 
-  CharVec *message = vector_init(MSG_BUFFER_SIZE);
   do {
     bytesRead = read(connection->fd, msgBuffer, MSG_BUFFER_SIZE);
     if (bytesRead > 0) {
@@ -64,15 +76,17 @@ int main(int argc, char **argv) {
       }
     }
   } while (bytesRead == MSG_BUFFER_SIZE);
+
   if (vector_push('\0', message)) {
     fprintf(stderr, "Error adding null terminator to message.\n");
     return EXIT_FAILURE;
   }
 
   printf("%s", message->data);
+
   destroySocket(connection);
   vector_free(message);
   destroySocket(listen_socket);
 
-  return ERR_SUCCESS;
+  return exit_code;
 }
